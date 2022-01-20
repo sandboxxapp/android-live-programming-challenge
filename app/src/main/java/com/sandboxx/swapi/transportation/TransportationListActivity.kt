d290d81833ce939c555f79845b5c1c0e95a80317
hino b <hino.banzon@gmail.com>
2022-01-20 16:46:54 -0500

    running state

--- app/src/main/java/com/sandboxx/swapi/transportation/TransportationListActivity.kt
+++ app/src/main/java/com/sandboxx/swapi/transportation/TransportationListActivity.kt
@@ -2,15 +2,23 @@ package com.sandboxx.swapi.transportation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.sandboxx.swapi.databinding.ActivityMainBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.sandboxx.swapi.R
 
 class TransportationListActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: TransportationListAdapter
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        adapter = TransportationListAdapter()
+
+        recyclerView = findViewById(R.id.rc_transports)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
     }
 }
