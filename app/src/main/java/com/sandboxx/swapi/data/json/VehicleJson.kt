473c0a41edb9c7ff4e99e8557b971eb17da1c41e
hino b <hino.banzon@gmail.com>
2022-01-20 14:23:48 -0500

    wip

---
+++ app/src/main/java/com/sandboxx/swapi/data/json/VehicleJson.kt
@@ -0,0 +1,6 @@
+package com.sandboxx.swapi.data.json
+
+data class VehicleJson(val name: String,
+                       val model: String,
+                       val url: String,
+                       val cargoCapacity: String)
---
+++ app/src/main/java/com/sandboxx/swapi/transportation/TransportationListAdapter.kt
@@ -0,0 +1,4 @@
+package com.sandboxx.swapi.transportation
+
+class TransportationListAdapter {
+}
