a0f2c158a947c92b2f36f43955cab5213127550f
hino b <hino.banzon@gmail.com>
2022-01-20 13:04:45 -0500

    wip

---
+++ app/src/main/java/com/sandboxx/swapi/common/BaseRecyclerAdapter.kt
@@ -0,0 +1,54 @@
+package com.sandboxx.swapi.common
+
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
+
+abstract class BaseRecyclerAdapter<T> protected constructor()
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var list: MutableList<T>
+
+    init {
+        list = ArrayList()
+    }
+
+    fun setList(list: MutableList<T>) {
+        this.list = list
+    }
+
+    fun getItem(position: Int): T? {
+        return if (position < list.size) {
+            list[position]
+        } else null
+    }
+
+    fun add(item: T) {
+        val position = itemCount
+        list.add(item)
+        notifyItemInserted(position)
+    }
+
+    fun addAll(items: List<T>) {
+        val position = itemCount
+        list.addAll(items)
+        notifyItemRangeInserted(position, items.size)
+    }
+
+    fun remove(item: T) {
+        val position = list.indexOf(item)
+        if (position > -1) {
+            list.remove(item)
+            notifyItemRemoved(position)
+        }
+    }
+
+    fun clear() {
+        val itemCount = itemCount
+        list.clear()
+        notifyItemRangeRemoved(0, itemCount)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
--- app/src/main/java/com/sandboxx/swapi/transportation/TransportationListActivity.kt
+++ app/src/main/java/com/sandboxx/swapi/transportation/TransportationListActivity.kt
@@ -1,10 +1,10 @@
-package com.sandboxx.swapi
+package com.sandboxx.swapi.transportation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sandboxx.swapi.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class TransportationListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
